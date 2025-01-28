cc813bf3caa19a689aaa5bef086236120888b14e
Melanie Colanino <melanie.hanna@gmail.com>
2025-01-28 11:30:30 -0600

    adding project files

---
+++ Memory/Memory/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  Memory
+//
+//  Created by Melanie Colanino on 1/28/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        HStack {
+            CardView(isFaceUp: true)
+            CardView()
+            CardView()
+            CardView()
+            
+        }.foregroundColor(.orange)
+            .padding()
+    }
+}
+struct CardView: View {
+    var isFaceUp: Bool = false
+    
+    var body: some View {
+        ZStack (content: {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 12)
+                    .foregroundColor(.white)
+                RoundedRectangle(cornerRadius: 12)
+                    .strokeBorder(lineWidth:5)
+                Text("🙄").font(.largeTitle)
+            } else {
+                RoundedRectangle(cornerRadius: 12)
+            }
+        })
+    }
+}
+
+
+
+
+#Preview {
+    ContentView()
+}
---
+++ Memory/Memory/MemoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryApp.swift
+//  Memory
+//
+//  Created by Melanie Colanino on 1/28/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
