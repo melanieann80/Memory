07e129119e11e30c168dd47e12f92d87bf1e395e
Melanie Colanino <melanie.hanna@gmail.com>
2025-01-28 16:35:11 -0600

    added plus and minus card buttons

--- Memory/Memory/ContentView.swift
+++ Memory/Memory/ContentView.swift
@@ -8,15 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    let emojis = ["❤️","💙","💜","💚","🩵"]
-
+    let emojis = ["❤️","💙","💜","💚","🩵","💔","🖤","🧡","💛","🩶","🤎","💕"]
+    
+    @State var cardCount: Int = 4
+    
     var body: some View {
-        HStack {
-            ForEach(emojis.indices, id: \.self) { index in
-                CardView(content:emojis[index])
-            }
+        VStack {
+            HStack {
+                ForEach(0..<cardCount, id: \.self) { index in
+                    CardView(content:emojis[index])
+                }
+            } .foregroundColor(.orange)
+            HStack{
+                Button(action: {
+                    if cardCount > 1 {cardCount -= 1}
+                }, label: {
+                    Image(systemName:"rectangle.stack.badge.minus.fill")
+                })
+
+                Spacer()
+                
+                Button(action: {
+                    if cardCount < emojis.count {cardCount += 1}
+                }, label: {
+                    Image(systemName:"rectangle.stack.badge.plus.fill")
+                })
+                }
+            .imageScale(.large)
+            .font(.largeTitle)
         
-        }.foregroundColor(.orange)
+        }
+       
             .padding()
     }
 }
